c5a85bc09f21ec0267e0488246efd40d8aef370f
刘其炬 <410672@nd.com>
2018-04-02 20:35:58 +0800

    添加时间补0正则表达式

--- util.js
+++ util.js
@@ -9,3 +9,17 @@ function addURLParam(url, name, value) {
     url += encodeURIComponent(name) + '=' + encodeURIComponent(value);
     return url;
 }
+
+/**
+ * 处理时间的时候前面补0快捷方式
+ * 两位数字前面补0的正则表达
+ * @param {int} num 
+ * 使用示例：
+ * const month = ('0' + (new Date().getMonth() + 1)).replace(/0(\d{2})/, '$1')
+ * const date = ('0' + new Date().getDate()).replace(/0(\d{2})/, '$1')
+ * const hour = ('0' + new Date().getHour()).replace(/0(\d{2})/, '$1')
+ * const minute = ('0' + new Date().getMinutes()).replace(/0(\d{2})/, '$1')
+ */
+function fixNumberStr(num) {
+    return `0${num}`.replace(/0(\d{2})/, '$1')
+}
